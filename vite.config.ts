db2edcdac9bbee74db0f8f4b07a5352d88dfb73c
Aprajita <aprajitakumarithakur5555@gmail.com>
2026-04-06 00:13:29 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://midnight-maps.onrender.com',
         changeOrigin: true,
       }
     }
